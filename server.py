0addbdce5c042716428537e639159611bc7f3954
ImUnsmart <devin0309@yahoo.com>
2021-06-04 16:52:13 -0400

    possible scroll fix

--- server.py
+++
@@ -1,8 +0,0 @@
-from http import HTTPServer, BaseHTTPRequestHandler
-
-def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    server_address = ('localhost', 8000)
-    httpd = server_class(server_address, handler_class)
-    httpd.serve_forever()
-
-if __name__ == "__main__":
