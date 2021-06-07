846584fd771a803861559c494967a7b0fe914f99
ImUnsmart <devin0309@yahoo.com>
2021-06-07 01:59:04 -0400

    buttons and landing

--- handler.js
+++ handler.js
@@ -1,4 +1,16 @@
+window.onload = function() {
+    document.body.style.setProperty('--scroll', 0);
+    document.getElementsByClassName('home-button')[0].getElementsByTagName('a')[0].style.setProperty('opacity', 'calc(max(var(--scroll) - 0.25, 0) * 4 / 3)');
+}
+
 window.addEventListener('scroll', () => {
     let scroll = window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
     document.body.style.setProperty('--scroll', scroll);
+
+    // handles the scroll up button
+    // if (scroll == 0) {
+    //     document.getElementsByClassName('home-button')[0].style.visibility = "hidden";
+    // } else {
+    //     document.getElementsByClassName('home-button')[0].style.visibility = "visible";
+    // }
 });
