38e493925cf42b24a59498f5b41220f46984f796
ImUnsmart <devin0309@yahoo.com>
2021-10-25 00:30:04 -0400

    button to allow disabling scroll

--- handler.js
+++ handler.js
@@ -1,16 +1,15 @@
-window.onload = () => {
-    document.body.style.setProperty('--scroll', 0);
-    document.getElementById("home-button").style.setProperty('opacity', 'calc(max(var(--scroll) - 0.25, 0) * 4 / 3)');
+
+const bgcheck = document.getElementById("background-button");
+
+/**
+ * Handles toggling scroll effect on checkbox press
+ */
+ bgcheck.onchange = () => {
+    for (let layer of document.getElementById("background-container").children) {
+        if (bgcheck.checked) {
+            layer.className += " noscroll";
+        } else {
+            layer.className = layer.className.substring(0, layer.className.length - 9);
+        }
+    }
 }
-
-window.addEventListener('scroll', () => {
-    let scroll = window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
-    document.body.style.setProperty('--scroll', scroll);
-
-    // handles the scroll up button
-    // if (scroll == 0) {
-    //     document.getElementsByClassName('home-button')[0].style.visibility = "hidden";
-    // } else {
-    //     document.getElementsByClassName('home-button')[0].style.visibility = "visible";
-    // }
-});
